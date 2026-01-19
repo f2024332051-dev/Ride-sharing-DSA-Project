4e61a88ddfe43651022f0102a20fce4dacaca110
LofixEnd <rayyanamjad313@gmail.com>
2026-01-19 21:01:59 +0500

    Add files via upload

---
+++ Rider.py
@@ -0,0 +1,10 @@
+"""
+Rider.py - Represents a rider in the system
+"""
+
+class Rider:
+    """Represents a rider"""
+    
+    def __init__(self, rider_id, name):
+        self.rider_id = rider_id
+        self.name = name
